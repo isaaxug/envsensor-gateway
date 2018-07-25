0c33720a11b5a56f94ce5a7439ab33708ab64742
Shoji Imamura <imamura@xshell.io>
2018-07-25 15:54:13 +0900

    remove waste code

--- client.py
+++ client.py
@@ -1,10 +1,7 @@
 import websocket
-# from threading import thread
 
 def on_message(ws, message):
-    # print(dir(message))
     print(message)
-    # print(message.decode())
 
 def on_error(ws, error):
     print(error)
@@ -12,22 +9,13 @@ def on_error(ws, error):
 def on_close(ws):
     print("### closed ###")
 
-# def on_open(ws):
-#     def run(*args):
-#         for i in range(3):
-#             time.sleep(1)
-#             ws.send("Hello %d" % i)
-#         time.sleep(1)
-#         ws.close()
-#         print("thread terminating...")
-#     thread.start_new_thread(run, ())
-
-
 if __name__ == "__main__":
+    import os
+    DEVICEID = os.environ.get('SOCKET_DEVICEID', 'E782E5311100')
+
     websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("ws://localhost:5000/talk/E782E5311100",
+    ws = websocket.WebSocketApp("ws://localhost:5000/talk/"+DEVICEID,
                               on_message = on_message,
                               on_error = on_error,
                               on_close = on_close)
-    # ws.on_open = on_open
     ws.run_forever()
